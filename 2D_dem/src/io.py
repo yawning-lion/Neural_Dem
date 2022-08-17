4b69f56e47c67c7dd24351c835534a85401cd60b
SunJN2000 <111328021+SunJN2000@users.noreply.github.com>
2022-08-17 15:32:38 +0800

    Create io.py

---
+++ 2D_dem/src/io.py
@@ -0,0 +1,25 @@
+import jax.numpy as np
+
+i32 = np.int32
+f64 = np.float64
+from matplotlib import pyplot as plt
+
+with open('V.npy', 'rb') as f:
+	v = np.load(f)*1*(2/3)
+
+# with open('V_list.npy', 'rb') as f:
+#     V = np.load(f)
+with open('test_z.npy', 'rb') as f:
+    z = np.load(f)
+T = np.power(z,2)
+T = np.sum(T,axis = 1).reshape(-1)*0.5
+t = np.arange(100000)
+
+plt.plot(t,T/T[0])
+plt.show()
+print(T[19999]/T[0])
+plt.plot(t,v/T[0])
+plt.show()
+
+plt.plot(t,(T+v)/T[0])
+plt.show()
